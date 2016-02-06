46644088b8399942905f25840e1ac50650b746d1
softvar <varun2902@gmail.com>
2016-02-06 15:12:33 +0530

    dynamic pre readme content

---
+++ pre-readme-content.js
@@ -0,0 +1,11 @@
+var readmeContent;
+
+readmeContent = '' +
+	'<p align="center">' +
+		'<img src="https://aws.producthunt.com/collection/15467/header_banner_collection_header_woman_in_tech.jpg" alt="Female Founders"/>' +
+		'<strong>Great stuff made by women 🙌</strong>' +
+	'</p>';
+
+readmeContent += '\n### List of products \n\n';
+
+exports.readmeContent = readmeContent;
