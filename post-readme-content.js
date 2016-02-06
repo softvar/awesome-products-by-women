eb6e7f1dacacc3254a43dd4948c307340e505450
softvar <varun2902@gmail.com>
2016-02-06 15:12:43 +0530

    dynamic post readme content

---
+++ post-readme-content.js
@@ -0,0 +1,40 @@
+var readmeContent;
+
+readmeContent = '\n\n' +
+'## Contribution' +
+'\n* Clone this repo' +
+'\n* Create a new branch(prefixing your github username). For eg. softvar-fixed-typo or softvar-updated-list' +
+'\n* Update the list of products or any cosmetic fix' +
+'\n* Please verify your changes.' +
+'\n* $ git commit -am "Added a new product - [product-name]"' +
+'\n* $ git push origin [branch-name]' +
+'\n* Open a Pull Request(PR)' +
+'\n\n' +
+'Please write a meaningful description for the PR and you\'re done.';
+
+
+readmeContent += '\n\n' +
+'## License' +
+'\n' +
+'The MIT License (MIT)' +
+'\n\nCopyright (c) 2016 Varun Malhotra' +
+'\n\n' +
+'Permission is hereby granted, free of charge, to any person obtaining a copy' +
+'of this software and associated documentation files (the "Software"), to deal' +
+'in the Software without restriction, including without limitation the rights' +
+'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell' +
+'copies of the Software, and to permit persons to whom the Software is' +
+'furnished to do so, subject to the following conditions:' +
+'\n\n' +
+'The above copyright notice and this permission notice shall be included in all' +
+'copies or substantial portions of the Software.' +
+'\n\n' +
+'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR' +
+'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,' +
+'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE' +
+'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER' +
+'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,' +
+'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE' +
+'SOFTWARE.';
+
+exports.readmeContent = readmeContent;
